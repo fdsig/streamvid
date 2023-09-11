8cb061cf7b237df0e90f6ec8f94ff37d119906e6
fdsig <fdesigley@gmail.com>
2023-09-11 23:36:07 +0100

    succesfull streaming in color

--- flask_vid_test.py
+++ flask_vid_test.py
@@ -4,12 +4,34 @@ from flask import Flask, Response, render_template
 app = Flask(__name__)
 
 # Capture video
-gst_str = (
-    "nvarguscamerasrc ! "
-    "video/x-raw(memory:NVMM),format=(string)NV12,width=1920,height=1080,framerate=30/1 ! "
-    "nvvidconv ! videoconvert ! "
-    "appsink"
-)
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=60,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+gst_str = gstreamer_pipeline()
 
 # Open capture
 cap = cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
