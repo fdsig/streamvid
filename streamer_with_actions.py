b15ca55ba48dfc16d3a4e37b19d82103b205749b
fdsig <fdesigley@gmail.com>
2023-10-03 13:55:29 +0100

    index page with buttons

---
+++ streamer_with_actions.py
@@ -0,0 +1,111 @@
+import cv2
+import time
+from flask import Flask, Response, render_template
+
+app = Flask(__name__)
+
+# Video state management class
+class VideoState:
+    def __init__(self):
+        self.is_streaming = True
+        self.is_paused = False
+        self.save_image = False
+
+video_state = VideoState()
+
+def gstreamer_pipeline(
+    capture_width=1280,
+    capture_height=720,
+    display_width=1280,
+    display_height=720,
+    framerate=10,
+    flip_method=0,
+):
+    return (
+        "nvarguscamerasrc ! "
+        "video/x-raw(memory:NVMM), "
+        "width=(int)%d, height=(int)%d, "
+        "format=(string)NV12, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (
+            capture_width,
+            capture_height,
+            framerate,
+            flip_method,
+            display_width,
+            display_height,
+        )
+    )
+
+gst_str = gstreamer_pipeline()
+
+# Open capture
+cap = cv2.VideoCapture(gst_str, cv2.CAP_GSTREAMER)
+
+def generate():
+    while True:
+        if video_state.is_streaming:
+            ret, frame = cap.read()
+            if not ret:
+                print("Error: Couldn't read frame.")
+                continue
+
+            if video_state.save_image:
+                cv2.imwrite("saved_image.jpg", frame)
+                video_state.save_image = False
+
+            if not video_state.is_paused:
+                ret, jpeg = cv2.imencode('.jpg', frame)
+                if not ret:
+                    print("Error: Couldn't encode frame.")
+                    continue
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
+            else:
+                time.sleep(0.1)  # Sleep for a bit when paused
+        else:
+            time.sleep(0.1)  # Sleep for a bit when streaming is stopped
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/start')
+def start_stream():
+    video_state.is_streaming = True
+    return "Stream started"
+
+@app.route('/stop')
+def stop_stream():
+    video_state.is_streaming = False
+    return "Stream stopped"
+
+@app.route('/pause')
+def pause_stream():
+    video_state.is_paused = True
+    return "Stream paused"
+
+@app.route('/resume')
+def resume_stream():
+    video_state.is_paused = False
+    return "Stream resumed"
+
+@app.route('/save_image')
+def save_current_image():
+    video_state.save_image = True
+    return "Image saved"
+
+@app.route('/via')
+def via():
+    return render_template('via.html')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=9080)
